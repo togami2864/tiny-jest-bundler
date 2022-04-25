bff7b9d1436feba900365cb13a2e334384ad4abe
togami2864 <tuabobo123@gmail.com>
2022-04-25 17:51:22 +0900

    add compress

--- __test__/cache.test.js
+++ __test__/cache.test.js
@@ -12,20 +12,39 @@ describe("cache", () => {
   });
   describe("write", () => {
     it("should write a cache file correctly", async () => {
-      const filePath = path.join(CACHE_DIR, "sample.json");
+      const filePath = path.join(CACHE_DIR, "sample");
       await write(
         filePath,
         {
           code: 'module.exports = "hello";',
         },
-        CACHE_DIR
+        CACHE_DIR,
+        false
       );
-      expect(fs.existsSync(filePath)).toBe(true);
+      expect(fs.existsSync(filePath + ".json")).toBe(true);
+    });
+    it("should compress", async () => {
+      const filename = "sample.json.gz";
+      const filePath = path.join(CACHE_DIR, "sample");
+      await write(
+        filePath,
+        {
+          code: 'module.exports = "hello";',
+        },
+        CACHE_DIR,
+        true
+      );
+      console.log(path.join(filePath, filename));
+      expect(fs.existsSync(filePath + ".json" + ".gz")).toBe(true);
     });
   });
   describe("read", () => {
     it("should read the cache file correctly", async () => {
       const filePath = path.join(CACHE_DIR, "sample");
+      fs.writeFileSync(
+        filePath + ".json",
+        JSON.stringify({ code: 'module.exports = "hello";' })
+      );
       const result = await read(filePath, CACHE_DIR);
       expect(result).toMatchObject(
         expect.objectContaining({
